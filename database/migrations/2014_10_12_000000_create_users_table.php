5b253b55ab8b92609af1374a057d7538d26e1725
Harun-or-rashid <harun35-2093@diu.edu.bd>
2021-05-08 14:40:58 +0600

    migration changed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('role')->comment('1=admin,2=volunteer');
+            $table->string('nid');
             $table->string('email',50)->unique();
             $table->string('phone');
             $table->tinyInteger('status')->default(0);
