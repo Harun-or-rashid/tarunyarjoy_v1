fe4db714e3b8c2a5e576ad5cb88d72e9704290d9
bappi2097 <bappi35-2097@diu.edu.bd>
2021-06-17 01:15:56 +0600

    prevent back history added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,9 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+
+        /* -------------------------- Prevent Back History -------------------------- */
+        'prevent-back-history' => \App\Http\Middleware\PreventBackHistory::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', 'Frontend\HomeController@index')->name('landing');
 
 Auth::routes();
 
-Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'role:Admin|Volunteer']], function () {
+Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'role:Admin|Volunteer', 'prevent-back-history']], function () {
     Route::get('/', 'Backend\DashboardController@index')->name('dashboard');
 
     /* --------------------------- User Profile Start --------------------------- */
@@ -31,14 +31,14 @@ Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'rol
 
         Route::get('/', "Backend\UserController@index")->name('index');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::get('create', "Backend\UserController@create")->name('create');
             Route::post('/', "Backend\UserController@store")->name('store');
         });
 
         Route::get('/{user}', "Backend\UserController@show")->name('show');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::get('edit/{user}', "Backend\UserController@edit")->name('edit');
             Route::put('/{user}', "Backend\UserController@update")->name('update');
             Route::put('change-password/{user}', "Backend\UserController@changePassword")->name('change-password');
@@ -50,14 +50,14 @@ Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'rol
 
         Route::get('/', "Backend\EventController@index")->name('index');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::get('create', "Backend\EventController@create")->name('create');
             Route::post('/', "Backend\EventController@store")->name('store');
         });
 
         Route::get('/{event}', "Backend\EventController@show")->name('show');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::get('edit/{event}', "Backend\EventController@edit")->name('edit');
             Route::put('/{event}', "Backend\EventController@update")->name('update');
             Route::delete('/{event}', "Backend\EventController@destroy")->name('delete');
@@ -68,14 +68,14 @@ Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'rol
 
         Route::get('/', "Backend\ProductController@index")->name('index');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::get('create', "Backend\ProductController@create")->name('create');
             Route::post('/', "Backend\ProductController@store")->name('store');
         });
 
         Route::get('/{product}', "Backend\ProductController@show")->name('show');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::get('edit/{product}', "Backend\ProductController@edit")->name('edit');
             Route::put('/{product}', "Backend\ProductController@update")->name('update');
             Route::delete('/{product}', "Backend\ProductController@destroy")->name('delete');
@@ -90,7 +90,7 @@ Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'rol
         Route::post('store/{product}', "Backend\RequestController@store")->name('store');
         Route::get('/{productRequest}', "Backend\RequestController@show")->name('show');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::put('accept/{productRequest}', "Backend\RequestController@accept")->name('accept');
             Route::put('reject/{productRequest}', "Backend\RequestController@reject")->name('reject');
         });
@@ -102,7 +102,7 @@ Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'rol
 
         Route::get('/{user}', "Backend\MemberController@show")->name('show');
 
-        Route::group(["middleware" => 'role:Admin'], function () {
+        Route::group(["middleware" => 'role:Admin', 'prevent-back-history'], function () {
             Route::put('accept/{user}', "Backend\MemberController@accept")->name('accept');
             Route::put('reject/{user}', "Backend\MemberController@reject")->name('reject');
         });
@@ -111,7 +111,7 @@ Route::group(['as' => 'home.', 'prefix' => 'home', 'middleware' => ['auth', 'rol
 
 /* -------------------------- Stripe Donation Start ------------------------- */
 
-Route::group(['middleware' => ['role:Donor']], function () {
+Route::group(['middleware' => ['role:Donor', 'prevent-back-history']], function () {
     Route::get('donate', 'StripePaymentController@donate')->name('home.donate');
     Route::post('stripe', 'StripePaymentController@stripePost')->name('home.stripe.post');
     Route::get('volunteer', function () {
